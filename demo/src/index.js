d16af447b1d2d1e356ce56b9e40fb4688cb3049b
Yuki <18482177461@163.com>
2018-08-06 17:40:10 +0800

    content部分

--- demo/src/index.js
+++ demo/src/index.js
@@ -10,7 +10,8 @@ class Demo extends Component {
         current_position={2}
         titles={['tit0', 'tit1', 'tit2', 'tit3']}
         contanierStyle={{ width: 300 }}
-        currentBgColor={'#a3c'} />
+        currentBgColor={'#a3c'}
+        list={[[0, 0, 0], [1, 1, 1], [2, 2, 2, 2, 2, 2, 2, 2, 2], [3, 3, 3, 3]]} />
     </div>
   }
 }
--- src/index.js
+++ src/index.js
@@ -14,15 +14,17 @@ export default class extends Component {
     this.tabs = this.props.titles
     this.contanierStyle = this.props.contanierStyle ? this.props.contanierStyle : defaultContanierStyle
     this.currentBgColor = this.props.currentBgColor ? this.props.currentBgColor : defaultCurrentBgColor
+    this.list = this.props.list
   }
   componentDidMount() {
 
   }
-  changeTab(i) {
+  changeTab = (i) => {
     this.setState({ current_position: i })
   }
   render() {
     let { current_position } = this.state
+    let currentList = this.list[current_position]
     return (
       <div className="contanier" style={this.contanierStyle}>
         <ul className="tab_ul">
@@ -31,12 +33,16 @@ export default class extends Component {
               key={i}
               className={current_position == i ? "tab_li current" : 'tab_li'}
               style={{ backgroundColor: current_position == i ? this.currentBgColor : 'rgba(238,238,238,.5)' }}
-              onClick={() => { this.changeTab(i) }}>
+              onClick={this.changeTab.bind(this, i)}>
               {item}
             </li>
           })}
         </ul>
-        <div className="content"></div>
+        <div className="content">
+          <ul className="content_ul">
+            {currentList.map((item, i) => { return <li className="item_li" key={i}>{item}</li> })}
+          </ul>
+        </div>
       </div>
     )
   }
